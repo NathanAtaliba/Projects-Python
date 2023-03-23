f3a1f0f7da86ae000b2d196e6abf715cddcd78ba
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-03-22 23:56:46 -0300

    Relatorio_4

--- Relatorio_4/main.py
+++ Relatorio_4/main.py
@@ -1,1 +1,16 @@
-# main
+import save_json
+from save_json import writeAJson
+from ProductAnalyzer import ProductAnalyzer
+pd = ProductAnalyzer()
+json1 = pd.clienteB()
+json2 = pd.listaProdutos()
+json3 = pd.clienteMenosG()
+json4 = pd.produtoMenosV()
+save_json.writeAJson(json1,"json1")
+save_json.writeAJson(json2,"json2")
+save_json.writeAJson(json3,"json3")
+save_json.writeAJson(json4,"json4")
+
+
+
+
