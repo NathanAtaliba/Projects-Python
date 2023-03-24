f932dbffdf923bc74baaa150824653d7fd57fea7
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-03-23 22:28:32 -0300

    Relatorio_4

--- Relatorio_4/ProductAnalyzer.py
+++ Relatorio_4/ProductAnalyzer.py
@@ -1,39 +1,37 @@
 import save_json
 from database import Database
-
 db = Database(database="loja_de_roupas", collection="vendas")
 db.resetDatabase()
 class ProductAnalyzer():
-    def clienteB(self): #Retorne o total que o cliente “B” gastou
+    def clienteB(self): #Retorne o total que o cliente “B” gastou.
         json1 = db.collection.aggregate([
             {"$unwind": "$produtos"},
-            {"$group": {"_id": "$cliente_id",
-                        "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}},
-            {"$group": {"_id": 2, "media": {"$sum": "$total"}}}
+            {"$group": {"_id": {"cliente": "$cliente_id",
+                        "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}}},
+
         ])
         return json1
-
-    def produtoMenosV(self): #Retorne o produto menos vendido em todas as compras
+    def produtoMenosV(self): #Retorne o produto menos vendido em todas as compras.
         json2 = db.collection.aggregate([
             {"$unwind": "$produtos"},
-            {"$group": {"_id": "$cliente_id",
-                        "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}},
-            {"$group": {"_id": None, "media": {"$avg": "$total"}}}
-        ])
+            {"$group": {"_id": "$produtos.nome", "total": {"$sum": "$produtos.quantidade"}}},
+            {"$sort": {"total": 1}},
+            {"$limit": 1}
+])
         return json2
-    def clienteMenosG(self): #Encontre o cliente que menos gastou em uma única compra
+    def clienteMenosG(self): #Encontre o cliente que menos gastou em uma única compra.
         json3 = db.collection.aggregate([
-            {"$unwind": "$produtos"},
-            {"$group": {"_id": "$cliente_id",
-                        "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}},
-            {"$group": {"_id": None, "media": {"$avg": "$total"}}}
-        ])
+    {"$unwind": "$produtos"},
+    {"$group": {"_id": {"cliente": "$cliente_id", "data": "$data_compra"}, "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}},
+    {"$sort": {"_id.data": 1, "total": 1}},
+    {"$group": {"_id": "$_id.data", "cliente": {"$first": "$_id.cliente"}, "total": {"$first": "$total"}}}
+])
         return json3
     def listaProdutos(self): #Liste todos os produtos que tiveram uma quantidade vendida acima de 2 unidades
         json4 = db.collection.aggregate([
             {"$unwind": "$produtos"},
-            {"$group": {"_id": "$cliente_id",
-                        "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}},
-            {"$group": {"_id": None, "media": {"$avg": "$total"}}}
+            {"$match": {"produtos.quantidade": {"$gt": 2}}},
+            {"$group": {"_id": "$produtos.nome"}},
         ])
+
         return json4
--- Relatorio_4/main.py
+++ Relatorio_4/main.py
@@ -6,10 +6,10 @@ json1 = pd.clienteB()
 json2 = pd.listaProdutos()
 json3 = pd.clienteMenosG()
 json4 = pd.produtoMenosV()
-save_json.writeAJson(json1,"json1")
-save_json.writeAJson(json2,"json2")
-save_json.writeAJson(json3,"json3")
-save_json.writeAJson(json4,"json4")
+save_json.writeAJson(json1,"Total que o cliente “B” gastou")
+save_json.writeAJson(json2,"Produto menos vendido em todas as compras")
+save_json.writeAJson(json3,"Cliente que menos gastou em uma única compra")
+save_json.writeAJson(json4,"Produtos que tiveram uma quantidade vendida acima de 2 unidades")
 
 
 
