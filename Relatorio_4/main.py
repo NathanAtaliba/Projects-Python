8b1ccc2d58c62ccb46a0efe480dae3be39753c86
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-03-27 22:08:59 -0300

    Relatorio_4

--- Relatorio_4/main.py
+++ Relatorio_4/main.py
@@ -6,10 +6,10 @@ json1 = pd.clienteB()
 json2 = pd.listaProdutos()
 json3 = pd.clienteMenosG()
 json4 = pd.produtoMenosV()
-save_json.writeAJson(json1,"Total que o cliente “B” gastou")
-save_json.writeAJson(json2,"Produto menos vendido em todas as compras")
-save_json.writeAJson(json3,"Cliente que menos gastou em uma única compra")
-save_json.writeAJson(json4,"Produtos que tiveram uma quantidade vendida acima de 2 unidades")
+save_json.writeAJson(json1, "Total que o cliente “B” gastou")
+save_json.writeAJson(json2, "Produto menos vendido em todas as compras")
+save_json.writeAJson(json3, "Cliente que menos gastou em uma única compra")
+save_json.writeAJson(json4, "Produtos que tiveram uma quantidade vendida acima de 2 unidades")
 
 
 
