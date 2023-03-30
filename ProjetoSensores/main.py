c98f311c1e9d6760818a10336320e8e5eeffa6a2
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-03-29 22:17:04 -0300

    Projeto_Sensores

--- ProjetoSensores/main.py
+++ ProjetoSensores/main.py
@@ -6,12 +6,12 @@ from pymongo import MongoClient
 # configurações do MongoDB
 client = MongoClient('localhost', 27017)
 db = client.bancoiot
-collection = db.sensores
+sensores = db.sensores
 
 # definição das classes de sensores
 class Sensor(threading.Thread):
-    def _init_(self, nome):
-        threading.Thread._init_(self)
+    def __init__(self, nome):
+        threading.Thread.__init__(self)
         self.nome = nome
         self.valor = 0
         self.alarmado = False
@@ -29,11 +29,10 @@ class Sensor(threading.Thread):
     def atualizar_db(self):
         query = {"nomeSensor": self.nome}
         update = {"$set": {"valorSensor": self.valor}}
-        collection.update_one(query, update, upsert=True)
-
+        sensores.update_one(query, update, upsert=True)
         if self.alarmado:
             update = {"$set": {"sensorAlarmado": True}}
-            collection.update_one(query, update)
+            sensores.update_one(query, update)
 
 # criação dos objetos de sensores e documentos no banco de dados
 temp1 = Sensor("Temp1")
@@ -47,9 +46,10 @@ docs = [
 ]
 
 for doc in docs:
-    collection.update_one({"nomeSensor": doc["nomeSensor"]}, {"$setOnInsert": doc}, upsert=True)
+    sensores.update_one({"nomeSensor": doc["nomeSensor"]}, {"$setOnInsert": doc}, upsert=True)
 
 # inicialização das threads
 temp1.start()
 temp2.start()
 temp3.start()
+
