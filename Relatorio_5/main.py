386de81197636d33c6dd4b6520dd89c0676547f6
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-03-30 19:53:30 -0300

    Relatorio_5

--- Relatorio_5/main.py
+++ Relatorio_5/main.py
@@ -1,58 +1,9 @@
 from database import Database
+from Livro import livro
+from crud import livroCLI
+
 db = Database(database="Biblioteca", collection="livros")
-db.resetDatabase()
+livro = livro(database=db)
 
-print("Oque voce deseja fazer?")
-print("1 - Adicionar livro")
-print("2 - Atualizar livro")
-print("3 - Deletar livro")
-print("4 - Procurar livro")
-print("5 - Nada")
-entrada = int(input("Entre com o valor: "))
-def adicionarLivro():
-    print("Qual livro voce deseja adicionar?")
-    titulo = input("Entre com o titulo:")
-    autor = input("Entre com o autor: ")
-    ano = int(input("Entre com o ano: "))
-    preco = int(input("Entre com o preco: "))
-
-    return 0
-def atualizarLivro():
-    print("Qual livro voce deseja atualizar?")
-    titulo = input("Entre com o titulo que deseja modificar:")
-    tituloNovo = input("Entre com o titulo novo:")
-
-    return 0
-def deletarLivro():
-    print("Qual livro voce deseja deletar?")
-    titulo = input("Entre com o titulo:")
-
-    return 0
-def procurarLivro():
-    print("Qual livro voce deseja procurar?")
-    titulo = input("Entre com o titulo:")
-
-
-    return 0
-def nada():
-    print("Obrigado, volte sempre!")
-
-    return 0
-def default():
-
-    return 0
-def switch (entrada):
-    if(entrada == 1):
-        return adicionarLivro()
-    elif(entrada == 2):
-        return adicionarLivro()
-    elif(entrada == 3):
-        return adicionarLivro()
-    elif(entrada == 4):
-        return adicionarLivro()
-    elif (entrada == 5):
-        return nada()
-    else:
-        return default
-
-switch(entrada)
+livroCLI = livroCLI(livro)
+livroCLI.run()
