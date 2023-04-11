09c6816b2b08c633433b66d3429ef97469b52ac8
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-04-11 00:53:39 -0300

    ProjetoSensores

--- ProjetoSensores/main.py
+++ ProjetoSensores/main.py
@@ -3,10 +3,10 @@ import time
 import random
 from pymongo import MongoClient
 
-# configurações do MongoDB
-client = MongoClient('localhost', 27017) # Configura a porta do servidor
-db = client.bancoiot # inicializa db como cliente.bancoiot
-sensores = db.sensores # sensores sera a colections sensores do bancoiot
+#Configurações do MongoDB
+client = MongoClient('localhost', 27017) #Configura a porta do servidor
+db = client.bancoiot #inicializa db como cliente.bancoiot
+sensores = db.sensores #sensores sera a colections sensores do bancoiot
 
 # definição das classes de sensores
 class Sensor(threading.Thread): # Criando a classe sensor
@@ -15,16 +15,15 @@ class Sensor(threading.Thread): # Criando a classe sensor
         self.nome = nome
         self.valor = 0
         self.alarmado = False
-
     def run(self):  # rodando a thread e gerando um numero aleatorio
         while True:
-            self.valor = round(random.uniform(30, 40), 2)
+            self.valor = round(random.uniform(30, 40), 2) #gerando valor de 30 a 40 para temperatura
             print(f"{self.nome}: {self.valor} C°")
             self.atualizar_db()
             if self.valor > 38 and not self.alarmado:
                 self.alarmado = True
                 print(f"Atenção! Temperatura muito alta! Verificar {self.nome}!")
-            time.sleep(5)
+            time.sleep(8) #Tempo que usuario deseja pare leitura dos sensores
 
     def atualizar_db(self):  # atualizando no documento o valor do sensor e se esta alarmado
         query = {"nomeSensor": self.nome}
@@ -39,10 +38,22 @@ temp1 = Sensor("Temp1")
 temp2 = Sensor("Temp2")
 temp3 = Sensor("Temp3")
 
+#Documento para manipulação
 documento = [
-    {"nomeSensor": "Temp1", "valorSensor": 0, "unidadeMedida": "C°", "sensorAlarmado": False},
-    {"nomeSensor": "Temp2", "valorSensor": 0, "unidadeMedida": "C°", "sensorAlarmado": False},
-    {"nomeSensor": "Temp3", "valorSensor": 0, "unidadeMedida": "C°", "sensorAlarmado": False},
+    {"nomeSensor": "Temp1",
+     "valorSensor": 0,
+     "unidadeMedida": "C°",
+     "sensorAlarmado": False},
+
+    {"nomeSensor": "Temp2",
+     "valorSensor": 0,
+     "unidadeMedida": "C°",
+     "sensorAlarmado": False},
+
+    {"nomeSensor": "Temp3",
+     "valorSensor": 0,
+     "unidadeMedida": "C°",
+     "sensorAlarmado": False},
 ]
 
 for doc in documento:
