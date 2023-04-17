b2911921e319dad717f15e06b718e426f9e9c6ef
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-04-17 18:36:06 -0300

    ExercicioProposto

---
+++ ExercicioProposto/Cuidador.py
@@ -0,0 +1,6 @@
+class Cuidador:
+    def __init__(self, id: str, nome: str, documento: str):
+        self.id = id
+        self.nome = nome
+        self.documento = documento
+
--- ExercicioProposto/main.py
+++ ExercicioProposto/main.py
@@ -1,1 +1,9 @@
-#EXERCICIO PROPOSTO
+from database import Database
+from Animal import animal
+from ZoologicoDAO import ZoologicoCLI
+
+db = Database(database="Zoologico", collection="zoologico")
+animal = animal(database=db)
+
+zoologicoCLI = ZoologicoCLI(animal)
+zoologicoCLI.run()
