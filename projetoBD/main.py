826e716327bcb31e339ed4a335abb73978416775
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-05-20 00:00:44 -0300

    RelatorioBD

--- projetoBD/main.py
+++ projetoBD/main.py
@@ -2,7 +2,7 @@ from database import Database
 from Consulta import BancoDeDados
 
 # cria uma instância da classe Database, passando os dados de conexão com o banco de dados Neo4j
-db = Database("bolt://54.173.102.236:7687", "neo4j", "conjectures-taste-lumps")
+db = Database("bolt://44.210.126.150:7687","neo4j", "oil-wires-passages")
 db.drop_all()
 
 # Criando uma instância da classe SchoolDatabase para interagir com o banco de dados
@@ -37,32 +37,35 @@ banco.criar_pessoa_pessoa("Fernanda", "Ian", "PAI_DE")
 banco.criar_pessoa_pessoa("Joao", "Fernanda", "ESPOSO_DE")
 banco.criar_pessoa_pessoa("Luzia", "Fernanda", "PAI_DE")
 
-# Print de todas as informações do banco de dados
-print("Oque deseja fazer?")
-print("0-Mostrar pessoas")
-print("1-Mostrar pets")
-print("2-Procurar quem tem algum emprego especifico")
-print("3-Procurar se uma pessoa é filha de alguem")
-print("4-Procurar quem é casada com tal pessoa")
-escolha = int(input("Escolha: "))
+while True:
+    # Print de todas as informações do banco de dados
+    print("Oque deseja fazer?")
+    print("0-Mostrar pessoas")
+    print("1-Mostrar pets")
+    print("2-Procurar quem tem algum emprego especifico")
+    print("3-Procurar se uma pessoa é filha de alguem")
+    print("4-Procurar quem é casada com tal pessoa")
+    print("5-Sair")
+    escolha = int(input("Escolha: "))
 
-def repeticao():
     if escolha == 0:
         print("Pessoas: ", banco.get_pessoa())
     elif escolha == 1:
         print("Pets: ", banco.get_pet())
-
     elif escolha == 2:
         nome = input("Entre com o nome do emprego: ")
         print("Quem é " + nome + " :", banco.get_emprego(nome))
-
     elif escolha == 3:
         nome = input("Entre com o nome do pai ou da mae: ")
         print("Quem tem " + nome + "como pai ou mae: ", banco.get_Pai(nome))
     elif escolha == 4:
         nome = input("Entre com o nome da pessoa que voce quer saber do relacionamento: ")
         print("Essa pessoa tem relacionamento com: ", banco.get_Relacionamento(nome))
+    elif escolha == 5:
+        print("Até mais!")
+        break
+    else:
+        print('Opção não existe!')
 
-repeticao()
 # Fechando a conexão
 db.close()
